b803618a398edda5741ef2e724c8fc95795f51b7
root <root@chandrareddy6.mylabserver.com>
2017-05-04 09:40:39 +0000

    initial java commit

---
+++ src/rectangle.java
@@ -0,0 +1,18 @@
+public class Rectangle {
+ public int length;
+ public int width;
+ 
+ public Rectangle(int length, int width){
+   this.length = length;
+   this.width = width;
+  }
+ 
+ public int getArea() {
+  this.length = length;
+  this.width = width;
+ }
+
+ public int getArea() {
+   return 2 * (length + width);
+  }
+ }
